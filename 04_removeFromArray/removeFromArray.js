4d83a6075335fbb3e4536d5e08708f2efee18c5b
euphoriccheckers <98302222+euphoriccheckers@users.noreply.github.com>
2023-10-11 19:43:14 -0700

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...Args) {
+    for(let arg in Args) {
+        let index = array.indexOf(Args[arg]);
 
+        if(index > -1) {
+            array.splice(index, 1);
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
