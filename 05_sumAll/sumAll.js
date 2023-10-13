c1be54967cae89715e059ab061e0c4aedcd8ea98
euphoriccheckers <98302222+euphoriccheckers@users.noreply.github.com>
2023-10-13 11:03:08 -0700

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    //filter non-numbers
+    if(typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return 'ERROR';
+    }
+    //filter negative numbers
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
+    //work with bigger number first
+    if(num1 > num2) {
+        let tempNum = num1;
+        num1 = num2;
+        num2 = tempNum;
+    }
 
+    for(let i = num1; i <= num2; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
