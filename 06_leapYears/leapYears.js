7498ad37be64132d0914886861d43e2e343a5e7d
euphoriccheckers <98302222+euphoriccheckers@users.noreply.github.com>
2023-10-13 11:45:52 -0700

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year % 4 === 0 && year % 100 !== 0) {
+        return true;
+    } else if (year % 4 === 0 && year % 400 === 0) {
+        return true;
+    } else {
+        return false;
+    }
 
+    //divisble by 4
+    //not divisible by 100
+    //divisible by 400
 };
 
 // Do not edit below this line
