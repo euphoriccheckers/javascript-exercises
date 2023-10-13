fcd2a2c7527f4cab344b0958e1579996d2ee2ad0
euphoriccheckers <98302222+euphoriccheckers@users.noreply.github.com>
2023-10-13 13:31:46 -0700

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  //	x °F ≘ (x − 32) × 5/9 °C
+  temp = (temp - 32) * (5/9);
+  //to round decimal: Math.round(num*multiplier)/multiplier
+  return Math.round(temp * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  //	x °C ≘ (x × 9/5 + 32) °F
+  temp = temp * (9/5) + 32;
+  //to round decimal: Math.round(num*multiplier)/multiplier
+  return Math.round(temp * 10) / 10;
+
 };
 
 // Do not edit below this line
